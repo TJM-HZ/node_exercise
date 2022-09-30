507d8a760bd7bd89917605b5fbdb34d40de8ef73
TJM-HZ <90186564+TJM-HZ@users.noreply.github.com>
2022-09-30 19:29:45 +0200

    Test example

--- test/calc.test.js
+++ test/calc.test.js
@@ -1,12 +1,11 @@
 const calc = require('../calc');
-// const sub = require('../sum').sub;
 
-// Tests for sum
+// Test(s) for sum
 test('one plus two is three', () => {
     expect(calc.sum(1,2)).toBe(3);
 })
 
-// Tests for sub
-test('one minus two is -1', () => {
+// Test(s) for sub
+test('one minus two is negative one', () => {
     expect(calc.sub(1,2)).toBe(-1);
 })
